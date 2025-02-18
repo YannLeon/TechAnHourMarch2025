9c2fe9d1351b99d96b6ff1ddda7fbeebc395c819
Yann LEON <Yann.Leon-ext@araymond.com>
2025-02-18 15:51:38 +0100

    really add

--- postit-dashboard-backend/server.js
+++ postit-dashboard-backend/server.js
@@ -1,6 +1,8 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import { addPostIt, getPostIts } from "./controllers/postit.controller.js";
+import { loginUser } from "./controllers/user.controller.js";
+import { registerUser } from "./services/user.service.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 // Routes
 app.get("/postits", getPostIts);
 app.post("/postits", addPostIt);
+
+app.post("/user/login", loginUser);
+app.post("/user/register", registerUser);
 
 // Start server
 app.listen(PORT, () => {
