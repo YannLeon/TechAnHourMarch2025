7c3635b6e1017e2788b1d623a0d7eaa2cee29e24
Marie Josée Bassil <mariejosebassil@gmail.com>
2025-03-02 18:30:52 +0100

    Log in test case

--- postit-dashboard-qa/cypress/e2e/login.feature
+++ postit-dashboard-qa/cypress/e2e/login.feature
@@ -2,3 +2,14 @@ Feature: Log in test cases
 
   Scenario Outline: Nominal case user log in
     Given I am on the log in page
+    When I fill in the log in form with the following credentials :
+      | username   | password   |
+      | <username> | <password> |
+    And I click on the log in button
+    Then I'm redirected to the home page
+
+    Examples:
+      | username | password |
+      | admin    | admin    |
+      | mj       | password |
+      | yann     |   123123 |
--- postit-dashboard-qa/cypress/support/step_definitions/login.js
+++ postit-dashboard-qa/cypress/support/step_definitions/login.js
@@ -1,5 +1,23 @@
-import { When, Then, Given, DataTable } from "@badeball/cypress-cucumber-preprocessor"
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
 
 Given("I am on the log in page", () => {
     cy.visit(Cypress.env("baseUrl"))
+    cy.url().should("include", '/authent')
+
 });
+
+When("I fill in the log in form with the following credentials :", (credentialsTable) => {
+  let credentials = credentialsTable.hashes();
+    credentials.forEach(credential => {
+        cy.get('[data-cy="auth-input-name"]').clear().type(credential.username)
+        cy.get('[data-cy="auth-input-password"]').clear().type(credential.password)
+    });
+})
+
+When("I click on the log in button", () => {
+  cy.get('[data-cy="auth-submit-btn"]').click()
+})
+
+Then("I'm redirected to the home page", () => {
+    cy.url().should("include", '/home')
+})
